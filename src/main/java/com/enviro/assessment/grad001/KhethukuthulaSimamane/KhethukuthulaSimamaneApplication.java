99746db11e20b41cb763fd894c3eaa48003d3fbf
Khetha100 <khethukuthula990@gmail.com>
2025-01-14 01:20:01 +0200

    my new spring boot repo

---
+++ src/main/java/com/enviro/assessment/grad001/KhethukuthulaSimamane/KhethukuthulaSimamaneApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.KhethukuthulaSimamane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhethukuthulaSimamaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhethukuthulaSimamaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/KhethukuthulaSimamane/KhethukuthulaSimamaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.KhethukuthulaSimamane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhethukuthulaSimamaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
